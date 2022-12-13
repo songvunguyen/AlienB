53fab0fb262a3b3559ff87e11d50487a9afe3614
Song Vu Nguyen <vunnems08@gmail.com>
2022-12-13 13:10:58 -0500

    Enable player to shoot projectile

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,6 +7,7 @@ public class PlayerController : MonoBehaviour
 {
     Rigidbody2D rb;
     Vector2 moveVal;
+    public GameObject projectile;
     float speed = 10f;
     void Start()
     {
@@ -20,6 +21,11 @@ public class PlayerController : MonoBehaviour
 
     void OnMove(InputValue value){
         moveVal = value.Get<Vector2>();
+    }
+
+    void OnFire(InputValue value){
+        GameObject g = Instantiate(projectile, transform.position + new Vector3(0.2f,0,0), projectile.transform.rotation);
+        g.GetComponent<Rigidbody2D>().velocity = new Vector2(10,0);
     }
 
 }
